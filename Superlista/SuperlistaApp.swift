a6cc54e4f04d380ede379037b053cf76d8bea22d
Marina De Pazzi <pazzi.dev@gmail.com>
2021-04-30 14:23:19 -0300

    chore-docs: added base Xcode project + added tests to gitignore

---
+++ Superlista/SuperlistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperlistaApp.swift
+//  Superlista
+//
+//  Created by Marina De Pazzi on 30/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SuperlistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
