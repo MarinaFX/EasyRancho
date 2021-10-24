d0d94672534c6b77a56b604c86b6574316d24f21
gabizorzo <gabriela.zorzo@edu.pucrs.br>
2021-10-24 18:49:35 -0300

    ajusets alertas para salvar o colaborador da lista

--- Superlista/SuperlistaApp.swift
+++ Superlista/SuperlistaApp.swift
@@ -19,38 +19,81 @@ struct SuperlistaApp: App {
         WindowGroup {
             NavigationView {
                 ZStack {
-                    SplashView()
-                        .onOpenURL(perform: { url in
-                            guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
-                                  let host = components.host else {
-                                      print("Invalid URL")
-                                      return
-                                  }
-                            
-                            let deepLink = DeepLink(id: host)
-                            
-                            handleDeepLink(deepLink)
-                            
-                        })
+                    ZStack {
+                        MainView()
+                            .onOpenURL(perform: { url in
+                                guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
+                                      let host = components.host else {
+                                          print("Invalid URL")
+                                          return
+                                      }
+                                
+                                let deepLink = DeepLink(id: host)
+                                
+                                handleDeepLink(deepLink)
+                                
+                            })
+                            .alert(isPresented: $presentCollabAlert) {
+                                Alert(
+                                    title: Text(list?.name ?? "Lista"),
+                                    message: Text("Você foi convidado você para colaborar em uma lista. Deseja se tornar um colaborador desta lista?"),
+                                    primaryButton:  .default(
+                                        Text("Cancelar"),
+                                        action: {
+                                            presentCollabAlert = false
+                                        }),
+                                    secondaryButton: .default(
+                                        Text("Aceitar"),
+                                        action: {
+                                            let ds = DispatchSemaphore(value: 1)
+                                            ds.wait()
+                                            CKService.currentModel.saveListUsersList(listID: list!.id, key: .SharedWithMe) { result in
+                                                switch result {
+                                                case .success(let listID):
+                                                    let user = CKService.currentModel.user!
+                                                    var sharedWith = list!.sharedWith
+                                                    sharedWith.append(user)
+                                                    CKService.currentModel.updateListCollab(listID: listID, sharedWith: sharedWith) { result in
+                                                        presentCollabAlert = false
+                                                        ds.signal()
+                                                    }
+                                                case .failure:
+                                                    presentCollabAlert = false
+                                                    ds.signal()
+                                                    return
+                                                }
+                                            }
+                                        }
+                                    )
+                                )
+                            }
+                    }
                 }
-                .alert(isPresented: $presentCollabAlert) {
-                    Alert(
-                        title: Text(list?.name ?? "Lista"),
-                        message: Text("Você foi convidado você para colaborar em uma lista. Deseja se tornar um colaborador desta lista?"),
-                        primaryButton:  .default(
-                            Text("Cancel"),
-                            action: {
-                                presentCollabAlert = false
+                .alert(isPresented: $presentSharedAlert) {
+                    Alert(title: Text(list?.name ?? "Lista"),
+                          message: Text("Esta lista foi compartilhada com você. Deseja adicionar uma cópia desta lista?"),
+                          primaryButton: .default(
+                            Text("Cancelar"), action: {
+                                presentSharedAlert = false
                             }),
-                        secondaryButton: .default(
+                          secondaryButton: .default(
                             Text("Aceitar"),
                             action: {
-                                CKService.currentModel.saveListUsersList(listID: list!.id, key: .SharedWithMe) { result in
-                                    print(result)
+                                let newOwnerRef = CKRecord.Reference(recordID: CKService.currentModel.user!.id, action: .none)
+                                let newListLocal = CKListModel(name: list!.name ?? "Nova Lista", ownerRef: newOwnerRef, itemsString: list!.itemsString, sharedWithRef: [])
+                                CKService.currentModel.createList(listModel: newListLocal) { result in
+                                    switch result {
+                                    case .success (let newListID):
+                                        CKService.currentModel.saveListUsersList(listID: newListID, key: .MyLists) { result in
+                                            presentSharedAlert = false
+                                        }
+                                    case .failure:
+                                        presentSharedAlert = false
+                                        return
+                                    }
                                 }
-                                presentCollabAlert = false
                             }
-                        )
+                          )
                     )
                 }
             }
@@ -59,32 +102,6 @@ struct SuperlistaApp: App {
             .environmentObject(listsViewModel)
             .onAppear {
                 loadData()
-            }
-            .alert(isPresented: $presentSharedAlert) {
-                Alert(title: Text(list?.name ?? "Lista"),
-                      message: Text("Esta lista foi compartilhada com você. Deseja adicionar uma cópia desta lista?"),
-                      primaryButton: .default(
-                        Text("Cancelar"), action: {
-                            presentSharedAlert = false
-                        }),
-                      secondaryButton: .default(
-                        Text("Aceitar"),
-                        action: {
-                            let newOwnerRef = CKRecord.Reference(recordID: CKService.currentModel.user!.id, action: .none)
-                            let newListLocal = CKListModel(name: list!.name ?? "Nova Lista", ownerRef: newOwnerRef, itemsString: list!.itemsString, sharedWithRef: [])
-                            CKService.currentModel.createList(listModel: newListLocal) { result in
-                                switch result {
-                                case .success (let newListID):
-                                    CKService.currentModel.saveListUsersList(listID: newListID, key: .MyLists) { result in
-                                    }
-                                case .failure:
-                                    return
-                                }
-                            }
-                            presentSharedAlert = false
-                        }
-                      )
-                )
             }
         }
     }
