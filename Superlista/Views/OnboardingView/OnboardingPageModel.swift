9f0e901bee7dce499e00e48a19da8a69a53cb110
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-10-27 15:50:06 -0300

    added localizable comments

--- Superlista/Views/OnboardingView/OnboardingPageModel.swift
+++ Superlista/Views/OnboardingView/OnboardingPageModel.swift
@@ -9,21 +9,21 @@ struct OnboardingPage {
 
 let onboardingPages = [
     OnboardingPage(
-        image: NSLocalizedString("OnboardingA", comment: ""),
-        title: NSLocalizedString("OnboardingATitle", comment: ""),
-        text: NSLocalizedString("OnboardingAText", comment: ""),
+        image: NSLocalizedString("OnboardingA", comment: "Onboarding A Image"),
+        title: NSLocalizedString("OnboardingATitle", comment: "Onboarding A Title"),
+        text: NSLocalizedString("OnboardingAText", comment: "Onboarding A Text"),
         color: Color("Background")
     ),
     OnboardingPage(
-        image: NSLocalizedString("OnboardingB", comment: ""),
-        title: NSLocalizedString("OnboardingBTitle", comment: ""),
-        text: NSLocalizedString("OnboardingBText", comment: ""),
+        image: NSLocalizedString("OnboardingB", comment: "Onboarding B Image"),
+        title: NSLocalizedString("OnboardingBTitle", comment: "Onboarding B Title"),
+        text: NSLocalizedString("OnboardingBText", comment: "Onboarding B Text"),
         color: Color("Higieneebeleza")
     ),
     OnboardingPage(
-        image: NSLocalizedString("OnboardingC", comment: ""),
-        title: NSLocalizedString("OnboardingCTitle", comment: ""),
-        text: NSLocalizedString("OnboardingCText", comment: ""),
+        image: NSLocalizedString("OnboardingC", comment: "Onboarding C Image"),
+        title: NSLocalizedString("OnboardingCTitle", comment: "Onboarding C Title"),
+        text: NSLocalizedString("OnboardingCText", comment: "Onboarding C Text"),
         color: Color("Button")
     )
 ]
