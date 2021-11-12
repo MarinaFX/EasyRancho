af56e952f39c5a62ba936724661eed1dba392960
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2021-11-11 22:07:21 -0300

    chore-front: added dynamic types do Settings + Profile Editing screens

--- Superlista/Views/SettingsView/SettingLabel.swift
+++ Superlista/Views/SettingsView/SettingLabel.swift
@@ -8,90 +8,109 @@
 import SwiftUI
 
 struct SettingLabel: View {
+    @Environment(\.sizeCategory) var sizeCategory
+        
     @Binding var username: String
-    @State var showingSheet = false
     @Binding var picture: UIImage?
+    @State var showingSheet = false
+    
+    private var axes: Axis.Set {
+        return sizeCategory.isAccessibilityCategory ? .vertical : []
+    }
     
     var body: some View {
-        VStack(spacing: 10){
-            Button(action: {
-                showingSheet = true
-            }) {
-                HStack {
-                    Text("SettingLabelA")
-                        .foregroundColor(.primary)
-                    
-                    Spacer()
-                    
-                    Image(systemName: "pencil.circle.fill")
-                        .foregroundColor(.primary)
-                        .font(.system(size: 20.0, weight: .bold))
-                }
-                .padding(20)
-                .foregroundColor(.white)
-                .background(Color("ButtonBG"))
-                .cornerRadius(13)
-            }
-            .sheet(isPresented: $showingSheet) {
-                EditProfileView(showingSheet: $showingSheet, username: $username, picture: $picture)
-            }
-            
-// Premium desativado temporariamente
-            //            Button(action: {
-            //                print("Button tapped!")
-            //            }) {
-            //                HStack {
-            //                    Text("Premium")
-            //                        .foregroundColor(.primary)
-            //                    Spacer()
-            //                    Image(systemName: "crown.fill")
-            //                        .foregroundColor(.primary)
-            //                        .font(.system(size: 20.0, weight: .bold))
-            //                }
-            //                .padding(20)
-            //                .foregroundColor(.white)
-            //                .background(Color("ButtonBG"))
-            //                .cornerRadius(13)
-            //            }
-            Button(action: {
-                guard let instagram = URL(string: "https://www.instagram.com/easyrancho") else { return }
-                UIApplication.shared.open(instagram)
-            }) {
-                HStack {
-                    Text("SettingLabelB")
-                        .foregroundColor(.primary)
-                    Spacer()
-                    Image(systemName: "ellipsis.bubble.fill")
-                        .foregroundColor(.primary)
-                        .font(.system(size: 20.0, weight: .bold))
-                }
-                .padding(20)
-                .foregroundColor(.white)
-                .background(Color("ButtonBG"))
-                .cornerRadius(13)
-            }
-            Button(action: {
-                //Tá dando varias mensagens de 'erro' mas nada que se preocupar por enquanto #pas
-                guard let urlShare = URL(string: "https://apps.apple.com/br/app/easyrancho-lista-de-compras/id1568546773") else { return }
-                let activityVC = UIActivityViewController(activityItems: [NSLocalizedString("InviteMessage", comment: "InviteMessage"), urlShare], applicationActivities: nil)
-                UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+        ScrollView(axes, showsIndicators: false) {
+            VStack(spacing: sizeCategory.isAccessibilityCategory ? 20 : 10){
                 
-            }) {
+                Button(action: {
+                    showingSheet = true
+                }) {
                     HStack {
-                        Text("SettingLabelC")
+                        Text("SettingLabelA")
+                            .font(.body)
                             .foregroundColor(.primary)
                         
                         Spacer()
                         
-                        Image(systemName: "bolt.heart.fill")
+                        Image(systemName: "pencil.circle.fill")
                             .foregroundColor(.primary)
-                            .font(.system(size: 20.0, weight: .bold))
+                            .font(.title3)
                     }
                     .padding(20)
                     .foregroundColor(.white)
                     .background(Color("ButtonBG"))
                     .cornerRadius(13)
                 }
+                .sheet(isPresented: $showingSheet) {
+                    EditProfileView(showingSheet: $showingSheet, username: $username, picture: $picture)
+                }
+                
+                // Premium desativado temporariamente
+                //            Button(action: {
+                //                print("Button tapped!")
+                //            }) {
+                //                HStack {
+                //                    Text("Premium")
+                //                        .font(.body)
+                //                        .foregroundColor(.primary)
+                //
+                //                    Spacer()
+                //
+                //                    Image(systemName: "crown.fill")
+                //                        .foregroundColor(.primary)
+                //                        .font(.system(size: 20.0, weight: .bold))
+                //                }
+                //                .padding(20)
+                //                .foregroundColor(.white)
+                //                .background(Color("ButtonBG"))
+                //                .cornerRadius(13)
+                //            }
+                
+                Button(action: {
+                    guard let instagram = URL(string: "https://www.instagram.com/easyrancho") else { return }
+                    UIApplication.shared.open(instagram)
+                }) {
+                    HStack {
+                        Text("SettingLabelB")
+                            .font(.body)
+                            .foregroundColor(.primary)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "ellipsis.bubble.fill")
+                            .foregroundColor(.primary)
+                            .font(.title3)
+                    }
+                    .padding(20)
+                    .foregroundColor(.white)
+                    .background(Color("ButtonBG"))
+                    .cornerRadius(13)
+                }
+                
+                Button(action: {
+                    //Tá dando varias mensagens de 'erro' mas nada que se preocupar por enquanto #pas
+                    guard let urlShare = URL(string: "https://apps.apple.com/br/app/easyrancho-lista-de-compras/id1568546773") else { return }
+                    let activityVC = UIActivityViewController(activityItems: [NSLocalizedString("InviteMessage", comment: "InviteMessage"), urlShare], applicationActivities: nil)
+                    UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+                    
+                }) {
+                    HStack {
+                        Text("SettingLabelC")
+                            .font(.body)
+                            .foregroundColor(.primary)
+
+                        Spacer()
+                        
+                        Image(systemName: "bolt.heart.fill")
+                            .foregroundColor(.primary)
+                            .font(.title3)
+                    }
+                    .padding(20)
+                    .foregroundColor(.white)
+                    .background(Color("ButtonBG"))
+                    .cornerRadius(13)
+                }
+            }
         }
     }
 }
